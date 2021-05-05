cec62632dd8eb2d4f0b34b3a8ca0618ac1020ad3
YilinH <e502122008@gmail.com>
2021-05-05 16:36:07 +0800

    incorrect implementation

---
+++ demo39.js
@@ -0,0 +1,33 @@
+// incorrect implementation
+
+
+const fs = require('fs')
+const path1 = "datax\\info.txt"
+function FileObject() {
+    this.fileName = "";
+    this.file_exists = function (callback) {
+        console.log("about to open:", this.fileName)
+        fs.open(this.fileName, 'r', function (err, handle) {
+            if (err) {
+                console.log("can not open file", this.fileName)
+                callback(err)        
+                return;
+            }
+            console.log("the file we process is:", this.fileName)
+            console.log("now we can use file handler:", handle)
+            fs.close(handle, function () { })
+            callback(null, true);
+            
+        })
+        console.log("this will no longer exist")
+    }
+}
+const fo = new FileObject()
+fo.fileName = path1;
+fo.file_exists((error, results) => {
+    if (error) {
+        console.log("error in file_exists", JSON.stringify(error))
+        return
+    }
+    console.log("file exists!", results)
+})
